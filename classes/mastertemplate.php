070f70a443c5a0769786c93819435330cfb116f4
Kordan <kordan@mclink.it>
2016-06-28 14:18:40 +0200

    CONTRIB-6411 mod_surveypro: fixed plugin version in XML

--- classes/mastertemplate.php
+++ classes/mastertemplate.php
@@ -300,7 +300,7 @@ class mod_surveypro_mastertemplate extends mod_surveypro_templatebase {
         $uselesspluginfields[] = 'surveyproid';
         $uselesspluginfields[] = 'itemid';
 
-        $versiondisk = $this->get_plugin_versiondisk();
+        $pluginversion = $this->get_plugin_version();
 
         $where = array('surveyproid' => $this->surveypro->id);
         if ($visiblesonly) {
@@ -318,7 +318,7 @@ class mod_surveypro_mastertemplate extends mod_surveypro_templatebase {
             $xmlitem = $xmltemplate->addChild('item');
             $xmlitem->addAttribute('type', $itemseed->type);
             $xmlitem->addAttribute('plugin', $itemseed->plugin);
-            $xmlitem->addAttribute('version', $versiondisk["$itemseed->plugin"]);
+            $xmlitem->addAttribute('version', $pluginversion["$itemseed->type"]["$itemseed->plugin"]);
 
             // Surveypro_item.
             $xmltable = $xmlitem->addChild('surveypro_item');
