10b6d4472fe3f14c3b5cd2f86aa254ce7db85722
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2018-06-07 12:55:20 +0200

    some trivial fixes to make Css linter happy

--- field/select/form/itemsetup_form.php
+++ field/select/form/itemsetup_form.php
@@ -35,7 +35,7 @@ require_once($CFG->dirroot.'/mod/surveypro/field/select/lib.php');
  * @copyright 2013 onwards kordan <kordan@mclink.it>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class select_setupform extends mod_surveypro_itembaseform {
+class mod_surveypro_select_setupform extends mod_surveypro_itembaseform {
 
     /**
      * Definition.
