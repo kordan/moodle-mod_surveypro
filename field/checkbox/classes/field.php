740187fb229f790cfc22716272cefef352f823a3
Kordan <d.cordella@iss.it>
2021-06-17 11:50:02 +0200

    Removed optional params to avoid deprecation messages

--- field/checkbox/classes/field.php
+++ field/checkbox/classes/field.php
@@ -159,7 +159,7 @@ class surveyprofield_checkbox_field extends mod_surveypro_itembase {
      * @param int $itemid Optional item ID
      * @param bool $getparentcontent True to include $item->parentcontent (as decoded by the parent item) too, false otherwise
      */
-    public function __construct($cm, $surveypro, $itemid=0, $getparentcontent) {
+    public function __construct($cm, $surveypro, $itemid, $getparentcontent) {
         parent::__construct($cm, $surveypro, $itemid, $getparentcontent);
 
         // List of properties set to static values.
