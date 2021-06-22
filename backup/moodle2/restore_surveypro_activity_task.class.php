46bc4f49f06c2885dd0f7aff49221cbd2712b7eb
Kordan <d.cordella@iss.it>
2021-06-22 10:09:32 +0200

    swapped static and public declarations

--- backup/moodle2/restore_surveypro_activity_task.class.php
+++ backup/moodle2/restore_surveypro_activity_task.class.php
@@ -55,7 +55,7 @@ class restore_surveypro_activity_task extends restore_activity_task {
      * Define the contents in the activity that must be
      * processed by the link decoder
      */
-    static public function define_decode_contents() {
+    public static function define_decode_contents() {
         $contents = array();
 
         $contents[] = new restore_decode_content('surveypro', array('intro'), 'surveypro');
@@ -68,7 +68,7 @@ class restore_surveypro_activity_task extends restore_activity_task {
      * Define the decoding rules for links belonging
      * to the activity to be executed by the link decoder
      */
-    static public function define_decode_rules() {
+    public static function define_decode_rules() {
         $rules = array();
 
         $rules[] = new restore_decode_rule('SURVEYPROVIEWBYID', '/mod/surveypro/view.php?id=$1', 'course_module');
@@ -83,7 +83,7 @@ class restore_surveypro_activity_task extends restore_activity_task {
      * surveypro logs. It must return one array
      * of {@link restore_log_rule} objects
      */
-    static public function define_restore_log_rules() {
+    public static function define_restore_log_rules() {
         $rules = array();
 
         $rules[] = new restore_log_rule('surveypro', 'add', 'view.php?id={course_module}', '{surveypro}');
@@ -106,7 +106,7 @@ class restore_surveypro_activity_task extends restore_activity_task {
      * by the restore final task, but are defined here at
      * activity level. All them are rules not linked to any module instance (cmid = 0)
      */
-    static public function define_restore_log_rules_for_course() {
+    public static function define_restore_log_rules_for_course() {
         $rules = array();
 
         // Fix old wrong uses (missing extension).
