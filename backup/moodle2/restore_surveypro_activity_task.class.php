76111bf0cd2d90362554694a7324d2fdb12e864d
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2021-10-30 18:30:22 +0200

    Fix some small phpdocs details

--- backup/moodle2/restore_surveypro_activity_task.class.php
+++ backup/moodle2/restore_surveypro_activity_task.class.php
@@ -85,9 +85,9 @@ class restore_surveypro_activity_task extends restore_activity_task {
 
     /**
      * Define the restore log rules that will be applied
-     * by the {@link restore_logs_processor} when restoring
+     * by the {@see restore_logs_processor} when restoring
      * surveypro logs. It must return one array
-     * of {@link restore_log_rule} objects
+     * of {@see restore_log_rule} objects
      */
     public static function define_restore_log_rules() {
         $rules = array();
@@ -104,9 +104,9 @@ class restore_surveypro_activity_task extends restore_activity_task {
 
     /**
      * Define the restore log rules that will be applied
-     * by the {@link restore_logs_processor} when restoring
+     * by the {@see restore_logs_processor} when restoring
      * course logs. It must return one array
-     * of {@link restore_log_rule} objects
+     * of {@see restore_log_rule} objects
      *
      * Note this rules are applied when restoring course logs
      * by the restore final task, but are defined here at
