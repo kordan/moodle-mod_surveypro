a3c01cbab2ea129a76a3a144b0f268c03544baa9
Kordan <d.cordella@iss.it>
2022-04-28 06:25:52 +0200

    removed starting underscore
    Following code ckecker report,
    each instance of public static function _get_reason() {
    has been replaced by
    public static function get_reason() : string {
    
    In the frame of the same intervention
        // To provide php 5.6 (33_STABLE) and up support.
        use \core_privacy\local\legacy_polyfill;
    has been dropped too.

--- templatemaster/classes/privacy/provider.php
+++ templatemaster/classes/privacy/provider.php
@@ -29,14 +29,10 @@ defined('MOODLE_INTERNAL') || die();
 /**
  * Privacy Subsystem for template_templatemaster implementing null_provider.
  *
- * @package   mod_surveypro
  * @copyright 2018 onwards kordan <kordan@mclink.it>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements \core_privacy\local\metadata\null_provider {
-
-    // To provide php 5.6 (33_STABLE) and up support.
-    use \core_privacy\local\legacy_polyfill;
 
     /**
      * Get the language string identifier with the component's language
@@ -44,7 +40,7 @@ class provider implements \core_privacy\local\metadata\null_provider {
      *
      * @return  string
      */
-    public static function _get_reason() {
+    public static function get_reason() : string {
         return 'privacy:metadata';
     }
 }
