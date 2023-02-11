ac1f0751bb5b28550d4d80b6986063d3efadcbbb
Kordan <d.cordella@iss.it>
2023-02-11 08:26:47 +0100

    Deleted doubled activity information
    Activity information is now managed from moodle core and surveypro no longer needs to take care of it.

--- view_import.php
+++ view_import.php
@@ -96,12 +96,6 @@ if ($PAGE->user_allowed_editing()) {
 }
 
 echo $OUTPUT->header();
-echo $OUTPUT->heading(format_string($surveypro->name), 2, null);
-
-// Render the activity information.
-$completiondetails = \core_completion\cm_completion_details::get_instance($cm, $USER->id);
-$activitydates = \core\activity_dates::get_dates_for_module($cm, $USER->id);
-echo $OUTPUT->activity_information($cm, $completiondetails, $activitydates);
 
 new tabs($cm, $context, $surveypro, SURVEYPRO_TABSUBMISSIONS, SURVEYPRO_SUBMISSION_IMPORT);
 
