08bdca258e3311075549b91c0504c68a4b076382
Kordan <d.cordella@iss.it>
2023-11-30 12:06:29 +0100

    Changed release for surveypro master (with template)
    In the frame of this PR I also modified a report that was breaking a test in m403.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_surveypro';
 $plugin->maturity = MATURITY_BETA;
-$plugin->version = 2023112501;
-$plugin->release = '1.0';
-$plugin->requires = 2023042400;
+$plugin->version = 2023120101;
+$plugin->release = '4.3';
+$plugin->requires = 2023100900;
