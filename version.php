087f4e92055e8a186952d5f8b621b407ce881a97
Kordan <d.cordella@iss.it>
2023-12-08 19:29:58 +0100

    Increased version from 2023111102 to 2023111103
    because the first release I uploaded had, wrongly, this same version number.
    Now the upload page of the plugin database does not allow me to upload one more relese with the same veversion nor to drop the old version.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_surveypro';
 $plugin->maturity = MATURITY_BETA;
-$plugin->version = 2023111102;
+$plugin->version = 2023111103;
 $plugin->release = '4.1.2';
 $plugin->requires = 2022041900;
