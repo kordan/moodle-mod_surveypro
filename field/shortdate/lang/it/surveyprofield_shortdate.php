72249370eef61e47ea49353cf78b908d31f32737
Daniele Cordella <d.cordella@iss.it>
2025-07-09 15:20:15 +0200

    Few more italian strings

---
+++ field/shortdate/lang/it/surveyprofield_shortdate.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'surveyprofield_shortdate', language 'en'
+ *
+ * @package   surveyprofield_shortdate
+ * @subpackage shortdate
+ * @copyright 2013 onwards kordan <stringapiccola@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['invitemonth'] = 'Seleziona un mese';
+$string['inviteyear'] = 'Seleziona un anno';
+$string['lowerbound'] = 'Limite inferiore';
+$string['lowerbound_help'] = 'La data minima che un utente può indicare';
+$string['pluginname'] = 'Data breve';
+$string['restriction_lower'] = 'La risposta deve essere maggiore di {$a}';
+$string['restriction_lowerupper'] = 'La risposta deve essere compresa fra {$a->lowerbound} e {$a->upperbound}';
+$string['restriction_upper'] = 'La risposta deve essere minore o uguale a {$a}';
+$string['uerr_greaterthanmaximum'] = 'Data posteriore alla massima consentita';
+$string['uerr_lowerthanminimum'] = 'Data precedente alla minima consentita';
+$string['uerr_outofinternalrange'] = 'La data indicata non cade nell\'intervallo richiesto';
+$string['upperbound'] = 'Limite superiore';
+$string['userfriendlypluginname'] = 'Data (breve) [mm/yyyy]';
