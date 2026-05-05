91def9ba912770e572719ca5b8a3198fb6b0ff48
Kordan <d.cordella@iss.it>
2026-05-05 10:14:41 +0200

    Bump to 2026042002

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_surveypro';
 $plugin->maturity = MATURITY_BETA;
-$plugin->version = 2026042001;
+$plugin->version = 2026042002;
 $plugin->requires = 2025100600;
 $plugin->release = '5.2';
