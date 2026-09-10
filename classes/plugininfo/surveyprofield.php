6fb583e4da8c3a6f248c02be6cb0e197698b20a9
Kordan <d.cordella@iss.it>
2026-09-10 12:39:55 +0200

    Fixed eye no longer working in plugin manager

--- classes/plugininfo/surveyprofield.php
+++ classes/plugininfo/surveyprofield.php
@@ -24,9 +24,7 @@
 
 namespace mod_surveypro\plugininfo;
 
-use core_text,
-
-core\plugininfo\base, core_plugin_manager, moodle_url;
+use core_text, core\plugininfo\base, core_plugin_manager, moodle_url;
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -41,6 +39,34 @@ require_once(dirname(__FILE__) . '/../../lib.php');
  */
 class surveyprofield extends base
 {
+    /**
+     * Enable or disable a plugin of this type.
+     *
+     * @param string $pluginname The plugin name to enable/disable.
+     * @param int $enabled Whether the plugin should be enabled (1) or disabled (0).
+     * @return bool True if the config has changed, false otherwise.
+     */
+    public static function enable_plugin(string $pluginname, int $enabled): bool {
+        $plugin = 'surveyprofield_' . $pluginname;
+        $oldvalue = get_config($plugin, 'disabled');
+        $disabled = !$enabled;
+
+        if ($oldvalue !== false && $disabled == (bool) $oldvalue) {
+            return false;
+        }
+
+        if ($disabled) {
+            set_config('disabled', 1, $plugin);
+        } else {
+            unset_config('disabled', $plugin);
+        }
+
+        add_to_config_log('disabled', $oldvalue, (int) $disabled, $plugin);
+        core_plugin_manager::reset_caches();
+
+        return true;
+    }
+
     /**
      * Finds all enabled plugins, the result may include missing plugins.
      *
